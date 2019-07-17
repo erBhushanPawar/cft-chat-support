1dc4e094bb1b79410f518f21470f6c795a385a8a
Prashant Shinde <prashantns9@gmail.com>
2019-07-17 10:42:09 +0530

    corrected electron url

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const electron = require('electron');
 const path = require('path');
 const url = require('url');
 const { app, BrowserWindow} = electron;
-const webUrl = 'https://prashantcftchatbot.000webhostapp.com/';
+const webUrl = 'http://127.0.0.1:8080/index.html';
 app.on('ready', function () {
     contactsWindow = new BrowserWindow({
         height: 640,
